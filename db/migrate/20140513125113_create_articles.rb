08d15dc5642c6846f159ba717fbbee98d7532b00
Tom Dyer <tdyer@ourstage.com>
2014-05-13 08:56:14 -0400

    Added the Article resource via scaffold

---
+++ db/migrate/20140513125113_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :content
+      t.string :category
+      t.string :status, default: 'draft'
+
+      t.timestamps
+    end
+  end
+end
