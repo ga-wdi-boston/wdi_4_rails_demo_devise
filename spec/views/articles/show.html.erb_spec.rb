eebfcc84436c7b736829b601873d37db8b287a0c
Tom Dyer <tdyer@ourstage.com>
2014-05-14 14:22:58 -0400

    Added a Article resource using scaffold

---
+++ spec/views/articles/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "articles/show" do
+  before(:each) do
+    @article = assign(:article, stub_model(Article,
+      :title => "Title",
+      :content => "MyText",
+      :category => "Category",
+      :status => "Status"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Title/)
+    rendered.should match(/MyText/)
+    rendered.should match(/Category/)
+    rendered.should match(/Status/)
+  end
+end
