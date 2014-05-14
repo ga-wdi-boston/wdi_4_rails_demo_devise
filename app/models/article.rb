3aa96980ab199b5f781b75b2c5203a36b515ad19
Tom Dyer <tdyer@ourstage.com>
2014-05-14 10:00:11 -0400

    Added Article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,11 @@ class Article < ActiveRecord::Base
   STATUSES = %w{ draft review published }
 
   CATEGORIES = %w{ software hardware business}
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :status, inclusion: { in: STATUSES }
+  
 end
