e564e3c5c7999bf5f4f1d64919220ec61a1bde88
Tom Dyer <tdyer@ourstage.com>
2014-05-15 11:15:17 -0400

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
 Article.delete_all
 puts 'Creating Articles'
-33.times do |i|
+5.times do |i|
   Article.create!(title: Faker::Lorem.words(rand(1..4)).join(' '), content: Faker::Lorem.paragraphs(i % 5).join(' '), category: Article::CATEGORIES.sample, status: Article::STATUSES.sample)
 end
 
 User.delete_all
 puts 'Creating Users'
-User.create!(email: 'joe@example.com', password: 'password')
-User.create!(email: 'jill@example.com', password: 'password')
-User.create!(email: 'tom@example.com', password: 'password')
+joe = User.create!(email: 'joe@example.com', password: 'password')
+jill = User.create!(email: 'jill@example.com', password: 'password')
+tom = User.create!(email: 'tom@example.com', password: 'password')
 
-joe = User.first
 3.times do |i|
   joe.articles.create!(title: "joes_article_#{i}", content: Faker::Lorem.paragraphs(i % 5).join(' '), category: Article::CATEGORIES.sample, status: Article::STATUSES.sample)
 end
+
+
+jill.articles << Article.first
+jill.articles <<  Article.create(title: 'Heroku Quickstart', content: 'Setting up Heroku.', category: 'software')
